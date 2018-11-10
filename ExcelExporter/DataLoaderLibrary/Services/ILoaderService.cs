554ad745dd5eb4db1b5e86bff4ee60cbf9f3dfc1
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-10 22:21:40 +0300

    Востановление после неудачной отмены коммитов

---
+++ ExcelExporter/DataLoaderLibrary/Services/ILoaderService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DataLoaderLibrary.Services
+{
+    public interface ILoaderService
+    {
+        IEnumerable<dynamic> GetQueryResults(string sqlExpression);
+    }
+}
